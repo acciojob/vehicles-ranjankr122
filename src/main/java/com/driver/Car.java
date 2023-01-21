40128e7c9879ead4a3daace87fc70a4b2d279b84
Ranjan <rkram122@gmail.com>
2023-01-21 17:45:00 +0530

    solotion for context1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,17 +9,57 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+    public Car(String name, boolean isManual){
+        super(name);
+        this.isManual=isManual;
+
+    }
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels=wheels;
+        this.doors=doors;
+        this.gears=gears;
+        this.isManual=isManual;
+        this.type=type;
+        this.seats=seats;
+        this.currentGear=1;
     }
 
     public void changeGear(int newGear){
-
+        this.gears=newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        this.move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,110 @@ package com.driver;
 
 public class F1 extends Car {
 
+
+    @Override
+    public void setWheels(int wheels) {
+        super.setWheels(wheels);
+    }
+
+    @Override
+    public void setType(String type) {
+        super.setType(type);
+    }
+
+    @Override
+    public void setDoors(int doors) {
+        super.setDoors(doors);
+    }
+
+    @Override
+    public void setGears(int gears) {
+        super.setGears(gears);
+    }
+
+    @Override
+    public void setManual(boolean manual) {
+        super.setManual(manual);
+    }
+
+    @Override
+    public void setCurrentGear(int currentGear) {
+        super.setCurrentGear(currentGear);
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        super.setSeats(seats);
+    }
+
+    public F1(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name, wheels, doors, gears, isManual, type, seats);
+    }
+
+    @Override
+    public void changeGear(int newGear) {
+        super.changeGear(newGear);
+    }
+
+    @Override
+    public void changeSpeed(int newSpeed, int newDirection) {
+        super.changeSpeed(newSpeed, newDirection);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName() {
+        super.setName();
+    }
+
+    @Override
+    public int getCurrentSpeed() {
+        return super.getCurrentSpeed();
+    }
+
+    @Override
+    public void setCurrentSpeed(int currentSpeed) {
+        super.setCurrentSpeed(currentSpeed);
+    }
+
+    @Override
+    public int getCurrentDirection() {
+        return super.getCurrentDirection();
+    }
+
+    @Override
+    public void setCurrentDirection(int currentDirection) {
+        super.setCurrentDirection(currentDirection);
+    }
+
+    @Override
+    public void steer(int direction) {
+        super.steer(direction);
+    }
+
+    @Override
+    public void move(int speed, int direction) {
+        super.move(speed, direction);
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
     public F1(String name, boolean isManual) {
+
+        super(name,isManual);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed =getCurrentSpeed() +rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,7 +117,27 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            setGears(1);
+            stop();
             //Stop the car, set gear as 1
+        }
+        if (newSpeed>=1 && newSpeed<=50){
+            setGears(1);
+        }
+        if (newSpeed>=51 && newSpeed<=100){
+            setGears(2);
+        }
+        if (newSpeed>=101 && newSpeed<=150){
+            setGears(3);
+        }
+        if (newSpeed>=151 && newSpeed<=200){
+            setGears(4);
+        }
+        if (newSpeed>=201 && newSpeed<=250){
+            setGears(5);
+        }
+        if (newSpeed>250){
+            setGears(6);
         }
         //for all other cases, change the gear accordingly
 
